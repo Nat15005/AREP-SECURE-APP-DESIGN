ee9cffac431c4bff920865006b6ec71b25dd3198
Nat <laura.rojas-r@mail.escuelaing.edu.co>
2025-03-07 07:53:46 -0500

    inicial

---
+++ src/main/java/arep/SecureApplicationDesign/SecureApplicationDesignApplication.java
@@ -0,0 +1,13 @@
+package arep.SecureApplicationDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApplicationDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApplicationDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arep/SecureApplicationDesign/SecureApplicationDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package arep.SecureApplicationDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApplicationDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
